339c1772b0cddbccf2031b5a41dae63b8cc876e0
sibulele <smboyi2016@gmail.com>
2021-04-05 10:11:53 +0200

    Signup Cubit

---
+++ lib/screens/signup/cubit/signup_state.dart
@@ -0,0 +1,56 @@
+part of 'signup_cubit.dart';
+
+enum SignupStatus {initial, submitting, success, error }
+
+class SignupState extends Equatable{
+  final String username;
+  final String email;
+  final String password;
+  final SignupStatus status;
+  final Failure failure;
+
+  bool get isFormValid => username.isNotEmpty && email.isNotEmpty && password.isNotEmpty;
+
+  SignupState({
+    @required this.username,
+    @required this.email,
+    @required this.password,
+    @required this.status,
+    @required this.failure
+  });
+
+  factory SignupState.initial(){
+    return SignupState(
+      username: null,
+      email: null,
+      password: null,
+      status: null,
+      failure: const Failure(),
+    );
+  }
+
+  @override
+  bool get stringify => true;
+
+  @override
+  List<Object> get props => [username ,email, password, status, failure];
+
+  /// allows us to modify SignupState while still maintaining all the parts we don't want to modify
+  SignupState copyWith({
+    String username,
+    String email,
+    String password,
+    SignupStatus status,
+    Failure failure,
+  }){
+    return SignupState(
+      username: username ?? this.username,
+      email: email ?? this.email,
+      password: password ?? this.password,
+      status: status ?? this.status,
+      failure: failure ?? this.failure,
+    );
+  }
+
+}
+

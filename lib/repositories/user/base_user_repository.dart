e8787ad8e1e1f8fe5fd927c00c9541640cf2a763
sibulele <smboyi2016@gmail.com>
2021-04-07 02:36:07 +0200

    User Repository

---
+++ lib/repositories/user/base_user_repository.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_instagram/models/user_model.dart';
+
+abstract class BaseUserRepository{
+  Future<User> getUserWithId({String userId});
+  Future<void> updateUser({User user});
+}
---
+++ lib/repositories/user/user_repository.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_instagram/config/paths.dart';
+import 'package:flutter_instagram/models/user_model.dart';
+import 'package:flutter_instagram/repositories/repositories.dart';
+
+class UserRepository extends BaseUserRepository{
+  /// The 1st thing to do is to create the dependency on Firebase Firestore because
+  /// we are going to be accessing the data from this File
+
+  final FirebaseFirestore _firebaseFirestore;
+
+  UserRepository({FirebaseFirestore firebaseFirestore}) 
+      :_firebaseFirestore = firebaseFirestore ?? FirebaseFirestore.instance;
+
+  @override
+  Future<User> getUserWithId({@required String userId}) async{
+    final doc = await _firebaseFirestore.collection(Paths.users).doc(userId).get();
+    /// we convert the 'doc'(document) that we get from Firestore to our user model.
+    return doc.exists ? User.fromDocument(doc) : User.empty;
+  }
+
+  @override
+  Future<void> updateUser({@required User user}) async{
+    await _firebaseFirestore.collection(Paths.users).doc(user.id).update(user.toDocument());
+  }
+  
+}
